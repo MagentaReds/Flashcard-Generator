42feba843fad597a6471524e3ad19171f0570255
MagentaReds <grantodaniels@gmail.com>
2017-04-06 12:49:41 -0500

    Added error handling when making a new ClozeCard object

--- cli.js
+++ cli.js
@@ -8,12 +8,32 @@ var text2 = process.argv[4];
 
 if(typeCard==="basic"){
   var card= new BasicCard(text1,text2);
+
+  //console output of card
   console.log(card.getDisplayStr());
+
+  //Get relevant inforamtion directly from object
+  console.log();
   console.log(card.front);
   console.log(card.back);
 } else if(typeCard==="cloze"){
-  var card = new ClozeCard(text1, text2);
+  var card;
+
+  //ClozeCard will throw an error if the cloze is not in the text, or of the cloze is the empty string
+  try {
+    card = new ClozeCard(text1, text2);
+  } catch(err){
+    return console.log(err);
+  }
+
+  //console output of card
   console.log(card.getDisplayStr());
-  console.log(card.text);
+
+  //Get relevant information direclty from object
+  console.log();
+  console.log(card.fullText);
+  console.log(card.partialText);
   console.log(card.cloze);
+} else {
+  console.log("Invalid argument");
 }
